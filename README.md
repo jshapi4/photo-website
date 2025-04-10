5f93a8386acafd908d4d39c70c04b28cec96ce3e
J. Shapiro <jshapi4@wgu.edu>
2025-04-10 21:32:18 +0000

    deploy: 88ca1f15ca11a6c93d06622ada8a9874804d3acc

--- README.md
+++
@@ -1,3 +0,0 @@
-# Emi's Photo Website
-
-This project is for Emi's photography website, which started in April 2025.
--- backend/contact/serializers.py
+++
@@ -1,8 +0,0 @@
-# serializers.py
-from rest_framework import serializers
-from .models import ContactSubmission
-
-class ContactSubmissionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ContactSubmission
-        fields = ['first_name', 'last_name', 'email', 'phone', 'pricing_package', 'message']
