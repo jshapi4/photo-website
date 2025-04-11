7d11da07a3aa3c9cd50c2e9c7d2b4c9f99484785
jshapi4 <jshapi4@wgu.edu>
2025-04-10 20:05:18 -0700

    adds base def for config of vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   build: {
-    outDir: "dist", // Ensure the build output is directed to 'dist'
-    assetsDir: "assets", // Optionally configure the assets directory inside 'dist'
+    outDir: "dist",
+    assetsDir: "assets",
   },
 });
