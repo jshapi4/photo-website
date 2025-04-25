78eb51d1b4017f4e90f905694c3e1a05679d748c
jshapi4 <jshapi4@wgu.edu>
2025-04-25 11:43:04 -0700

    adds testimonial from Jessica

--- frontend/src/data/testimonialData.js
+++ frontend/src/data/testimonialData.js
@@ -6,13 +6,13 @@ const testimonials = [
       "Emi is an absolute gift! Our family photo session with you was nothing short of magical. Your sweet, warm personality instantly put us at ease. My one and a half year old son was so smitten with you that he let you carry him and was more affectionate than he is with most people. That alone speaks to the special energy you bring! You made the whole experience feel so natural and effortless, like you were just a friend hanging out with us, capturing our joy without ever intruding on the moment. Your kindness and personable nature made it such a delight to work with you. And when we saw the photos? Oh, Emi, they took my breath away. Your talent is truly remarkable. The way you captured my son’s spark and our family’s love was beyond anything I could have imagined. It’s rare to find someone who’s not only so skilled at their craft but also so genuinely good hearted. Emi, you’re a treasure, and we can’t wait to work with you again. Thank you for giving us memories we’ll cherish forever!",
     image: "/assets/CD-001.jpg",
   },
-  // {
-  //   name: "Laura R.",
-  //   position: "Expecting Mom",
-  //   quote:
-  //     "Emi made our maternity session so special. The photos are beautiful, and she really made me feel comfortable in front of the camera. I can't wait for our newborn session with her!",
-  //   image: "/images/laura-portrait.jpg",
-  // },
+  {
+    name: "Jessica",
+    position: "Mother",
+    quote:
+      "I was lucky to be one of the few families chosen for a photography session as this talented photographer builds her portfolio, and I’m so glad I took the opportunity. She made the entire experience easy and fun from start to finish. I especially appreciated how quickly she delivered the final photos—such a rare and refreshing quality! She also had a fantastic eye for natural, candid moments and offered creative pose ideas that brought out the best in our family. The photos truly captured who we are, and we’ll treasure them for years to come.",
+    image: "/assets/JK-004.JPEG",
+  },
   // {
   //   name: "Rebecca S.",
   //   position: "Mother of Two",
