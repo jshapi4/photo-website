010958fdb0fd846b9b33471e186cc6ad9f8991f4
jshapi4 <jshapi4@wgu.edu>
2025-04-25 14:35:32 -0700

    adds quote char

--- frontend/src/data/testimonialData.js
+++ frontend/src/data/testimonialData.js
@@ -3,21 +3,21 @@ const testimonials = [
     name: "Candace",
     position: "Family Session",
     quote:
-      "Emi is an absolute gift! Our family photo session with you was nothing short of magical. Your sweet, warm personality instantly put us at ease. My one and a half year old son was so smitten with you that he let you carry him and was more affectionate than he is with most people. That alone speaks to the special energy you bring! You made the whole experience feel so natural and effortless, like you were just a friend hanging out with us, capturing our joy without ever intruding on the moment. Your kindness and personable nature made it such a delight to work with you. And when we saw the photos? Oh, Emi, they took my breath away. Your talent is truly remarkable. The way you captured my son’s spark and our family’s love was beyond anything I could have imagined. It’s rare to find someone who’s not only so skilled at their craft but also so genuinely good hearted. Emi, you’re a treasure, and we can’t wait to work with you again. Thank you for giving us memories we’ll cherish forever!",
+      '"Emi is an absolute gift! Our family photo session with you was nothing short of magical. Your sweet, warm personality instantly put us at ease. My one and a half year old son was so smitten with you that he let you carry him and was more affectionate than he is with most people. That alone speaks to the special energy you bring! You made the whole experience feel so natural and effortless, like you were just a friend hanging out with us, capturing our joy without ever intruding on the moment. Your kindness and personable nature made it such a delight to work with you. And when we saw the photos? Oh, Emi, they took my breath away. Your talent is truly remarkable. The way you captured my son’s spark and our family’s love was beyond anything I could have imagined. It’s rare to find someone who’s not only so skilled at their craft but also so genuinely good hearted. Emi, you’re a treasure, and we can’t wait to work with you again. Thank you for giving us memories we’ll cherish forever!"',
     image: "/assets/CD-001.jpg",
   },
   {
     name: "Jessica",
     position: "Family Session",
     quote:
-      "[Emi] made the entire experience easy and fun from start to finish. I especially appreciated how quickly she delivered the final photos—such a rare and refreshing quality! She also had a fantastic eye for natural, candid moments and offered creative pose ideas that brought out the best in our family. The photos truly captured who we are, and we’ll treasure them for years to come.",
+      '"Emi made the entire experience easy and fun from start to finish. I especially appreciated how quickly she delivered the final photos—such a rare and refreshing quality! She also had a fantastic eye for natural, candid moments and offered creative pose ideas that brought out the best in our family. The photos truly captured who we are, and we’ll treasure them for years to come."',
     image: "/assets/JK-004.JPEG",
   },
   {
     name: "Paris",
     position: "Family Session",
     quote:
-      "We recently had the pleasure of having candid family photos taken by Emi at the UC Davis Arboretum, near the horses—and the experience was absolutely wonderful. She has such a natural eye for capturing genuine moments, and every photo reflects the love and connection in our little family. The setting was perfect, but it was truly her talent and calming presence that made the session feel so natural and like us. On top of that, her turnaround time was incredibly fast, and we were blown away by the quality of the final images. We will cherish these photos forever. I can’t recommend her enough!",
+      '"We recently had the pleasure of having candid family photos taken by Emi at the UC Davis Arboretum, near the horses—and the experience was absolutely wonderful. She has such a natural eye for capturing genuine moments, and every photo reflects the love and connection in our little family. The setting was perfect, but it was truly her talent and calming presence that made the session feel so natural and like us. On top of that, her turnaround time was incredibly fast, and we were blown away by the quality of the final images. We will cherish these photos forever. I can’t recommend her enough!"',
     image: "/assets/PM-005.jpg",
   },
   // {
